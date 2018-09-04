bc02199af9baf85cab7d6d95c30e228b48ca64f7
Stabmirz <amirza@techlaunch.io>
2018-09-04 18:23:22 -0400

    first commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,39 @@
 const dog_api = 'https://dog.ceo/api/'
+const selectBreed = document.querySelector("#selectBreed");
+const viewDog = document.querySelector("#viewDog");
+const breedImage = document.querySelector("#breedImage");
 
-function random(){
-    //This is the code
+function getDogBreeds() {
+    $.ajax({
+        url: `${dog_api}breeds/list`,
+        dataType: 'json',
+        success: (data => {
+            // console.log(data);
+            for (let i=0; i<data.message.length; i++) {
+                selectBreed.innerHTML += `
+            <option value= "${data.message[i]}">${data.message[i]}</option>
+            `
+            }
+        }),
+        error: error => {
+            console.log("There was an error")
+        }
+    })
 }
+getDogBreeds();
 
-viewDog.addEventListener('click', random())
+viewDog.addEventListener('click', e=>{
+    const breedName = selectBreed.value;
+    // console.log(breedName);
+    $.ajax({
+        url: `${dog_api}breed/${breedName}/images/random`,
+        dataType: 'json',
+        success: (data => {
+            // console.log(data);
+            breedImage.setAttribute("src", data.message);
+        }),
+        error: error => {
+            console.log("There was an error")
+        }
+    })
+})
